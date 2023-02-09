b499d043cb113801380d29f56e1b4e1424a53971
M  Dileep(192111648) <113426680+Dileep441@users.noreply.github.com>
2023-02-09 19:33:20 +0530

    Add files via upload

---
+++ 18.convert hexa to decimal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the hexa value:");
+	scanf("%x",&n);
+	printf("The decimal value is: %d",n);
+	return 0;
+}
---
+++ 19.decimal to octal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("\n  Enter the decial Number You want to Convert  :  ");
+    scanf("%d", &n);
+    
+    printf("\n Octal Number of a Given Number =  %o", n);
+
+    return 0;
+}
